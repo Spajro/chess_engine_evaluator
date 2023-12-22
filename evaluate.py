ab4c4b66deb94e2a9f53e57c08914227e1ac335f
Spajro <rafal.spyra@onet.pl>
2023-12-23 00:38:01 +0100

    add engine evaluation

---
+++ evaluate.py
@@ -0,0 +1,34 @@
+import sys
+
+import core
+
+if len(sys.argv) < 2:
+    print("SYNTAX: evaluate.py ENGINE_NAME")
+    exit(200)
+
+elo = 1500
+max_elo = 3000
+min_elo = 0
+engine = core.UciEngine(sys.argv[1])
+for ed in [500, 400, 300, 200, 100]:
+    wr, br = core.play_match(engine,
+                             core.StockfishEngine(elo),
+                             5,
+                             5 * 60 * 1000)
+
+    win = wr.count(1) + br.count(-1)
+    draw = wr.count(0) + br.count(0)
+    lose = wr.count(-1) + br.count(1)
+
+    rating_change = ed * (win - lose) / 10
+
+    print("Result: ", win, '-', draw, '-', lose, " vs elo:", elo, " rating change: ")
+
+    elo += rating_change
+
+    if rating_change > 0 and elo > min_elo:
+        min_elo = elo
+    if rating_change < 0 and elo < max_elo:
+        max_elo = elo
+
+print("Evaluated elo: ", elo, " min win: ", min_elo, " max lose: ", max_elo)
