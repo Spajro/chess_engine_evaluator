48bc3503240cadb9307ae52d1ac5955b406d44a2
Spajro <rafal.spyra@onet.pl>
2024-02-03 13:39:50 +0100

    change error messages in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from src.config import STOCKFISH, load_config
 
 if len(sys.argv) < 2:
-    print("No command provided")
+    print("USAGE: py setup.py stockfish STOCKFISH_PATH  |  py setup.py uci ENGINE_NAME ENGINE_PATH")
     exit(1)
 
 command = sys.argv[1]
@@ -12,13 +12,13 @@ tokens = sys.argv[2:]
 config = load_config()
 if command == "uci":
     if len(tokens) < 2:
-        print("Not enough tokens provided")
+        print("USAGE: py setup.py uci ENGINE_NAME ENGINE_PATH")
         exit(2)
     config[tokens[0]] = tokens[1]
 
 if command == "stockfish":
     if len(tokens) < 1:
-        print("Not enough tokens provided")
+        print("USAGE: py setup.py stockfish STOCKFISH_PATH")
         exit(3)
     config[STOCKFISH] = tokens[0]
 
