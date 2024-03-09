5f6035af2a21153f4db5c372774d84acbbb83e57
Spajro <rafal.spyra@onet.pl>
2024-03-09 12:48:26 +0100

    simplify solve_puzzle returns

--- src/puzzle.py
+++ src/puzzle.py
@@ -1,5 +1,5 @@
 import concurrent.futures
-from typing import Tuple, Optional
+from typing import Tuple
 
 from src.engines import Engine
 from src.flags import get_threads
@@ -18,30 +18,38 @@ class Puzzle:
         return "{" + self.fen + ",moves[" + ",".join(self.moves) + "]}"
 
 
-def solve_puzzle(engine: Engine, puzzle: Puzzle) -> Tuple[bool, 'Puzzle', Optional[list[str]]]:
+def solve_puzzle(engine: Engine, puzzle: Puzzle) -> Tuple[bool, list[str]]:
     engine.update_fen(puzzle.fen, [puzzle.moves[0]])
     move_time = 10 * 1000
     moves = [puzzle.moves[0]]
     i = 1
+
     while i < len(puzzle.moves):
         move = engine.make_move(move_time, move_time)
         moves.append(move)
+
         if move != puzzle.moves[i]:
             engine.quit()
-            return False, puzzle, moves
+            return False, moves
+
         i += 1
         if i == len(puzzle.moves):
             engine.quit()
-            return True, puzzle, None
+            return True, moves
+
         engine.update([puzzle.moves[i]])
         i += 1
+
     print("ERROR")
     engine.quit()
-    return False, puzzle, moves
+    return False, moves
 
 
-def solve_puzzles(engine: EngineTemplate, puzzles: [Puzzle]):
+def solve_puzzles(engine: EngineTemplate, puzzles: [Puzzle]) -> list[Tuple[bool, Puzzle, list[str]]]:
     data = [(engine.get_instance(), puzzle) for puzzle in puzzles]
+
     with concurrent.futures.ThreadPoolExecutor(threads) as executor:
-        futures = [executor.submit(solve_puzzle, e, p) for e, p in data]
-    return [f.result() for f in futures]
+        futures = [(executor.submit(solve_puzzle, e, p), p) for e, p in data]
+
+    result = [(f.result(), p) for f, p in futures]
+    return [(f[0], p, f[1]) for f, p in result]
