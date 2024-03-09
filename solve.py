9d25db31931066d32595beb46a146f8aaca8749f
Spajro <rafal.spyra@onet.pl>
2024-03-09 13:19:03 +0100

    add tag stats

--- solve.py
+++ solve.py
@@ -2,6 +2,7 @@ import sys
 
 from src.puzzle import solve_puzzles
 from src.puzzle_manager import load
+from src.tag_stats import TagStats
 from src.templates import UciEngineTemplate
 
 if len(sys.argv) < 2:
@@ -12,12 +13,17 @@ k = int(sys.argv[2])
 puzzles = load(k)
 result = solve_puzzles(UciEngineTemplate(sys.argv[1]), puzzles)
 score = 0
+tag_stats = TagStats()
 
 print("FAILED: ")
 for solved, puzzle, optional_moves in result:
     if solved:
         score += 1
+        tag_stats.add_puzzle(True, puzzle)
     else:
+        tag_stats.add_puzzle(False, puzzle)
         print(puzzle.__str__() + " | [" + ",".join(optional_moves) + "]")
 
 print("RESULT: " + str(score) + "/" + str(k))
+print("STATS FOR TAG:")
+print(tag_stats)
---
+++ src/tag_stats.py
@@ -0,0 +1,26 @@
+from src.puzzle import Puzzle
+
+
+class TagStats:
+    def __init__(self):
+        self.tag_wins = dict()
+        self.tag_loses = dict()
+        self.tags = set()
+
+    def __str__(self):
+        result = ""
+        for tag in self.tags:
+            result += tag + " " + str(self.tag_wins[tag]) + " / " + str(self.tag_wins[tag] + self.tag_loses[tag]) + "\n"
+        return result
+
+    def add_puzzle(self, success: bool, puzzle: Puzzle):
+        for tag in puzzle.tags:
+            self.tags.add(tag)
+            if tag not in self.tag_wins:
+                self.tag_wins[tag] = 0
+            if tag not in self.tag_loses:
+                self.tag_loses[tag] = 0
+            if success:
+                self.tag_wins[tag] += 1
+            else:
+                self.tag_loses[tag] += 1
